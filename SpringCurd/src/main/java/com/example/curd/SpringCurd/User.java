6c73f9148ed8c3a6f65fe7f60ac1ffcb05590fd6
Yashaswini09 <42270875+Yashaswini09@users.noreply.github.com>
2019-05-08 15:05:25 +0530

    Add files via upload

---
+++ SpringCurd/src/main/java/com/example/curd/SpringCurd/User.java
@@ -0,0 +1,84 @@
+package com.example.curd.SpringCurd;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Entity
+@Table
+public class User {
+     
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+     
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+     
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+    
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + "]";
+	}
+
+	public User(long id, @NotBlank(message = "Name is mandatory") String name,
+			@NotBlank(message = "Email is mandatory") String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+
+	
+   
+}
+
+
+
+
+
+
+
+
+
+
+
